a95debf8f260a50e7fa5fc0e67154d19f6883601
yrwww <yaorongwei03@gmail.com>
2025-03-12 20:29:59 +0800

    Site updated: 2025-03-12 20:29:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/12/学成在线/"];function toRandomPost(){
+var posts=["2025/03/12/学成在线/","2025/03/12/二分查找/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
